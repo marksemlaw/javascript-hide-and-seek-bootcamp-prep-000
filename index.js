26593e5ccb469dbeb13d771e5343cdf85d5a6323
Mark Semotiuk <marksem@gmail.com>
2018-08-02 00:32:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('div#nested .target')
+}
+
+function increaseRankBy(n){
+  const initialrank = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < initialrank.length; i++) {
+  initialrank[i].innerHTML = (i + 1).toString()
+}}
+
+function deepestChild(){
+  let id = 'div#grand-node'
+  let div = ' div'
+  let query = document.querySelectorAll(id)
+  while (query){
+    id + div
+  }
+  return query
+}
